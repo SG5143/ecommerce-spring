ddfcc1474d18d58b42be3dd434d4f50c9fc57035
SG5143 <sun5143a@gmail.com>
2026-07-02 11:31:32 +0900

    feat: 초기 프로젝트 설정

---
+++ src/main/java/com/lsg/mingler/MinglerApplication.java
@@ -0,0 +1,13 @@
+package com.lsg.mingler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinglerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinglerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lsg/mingler/MinglerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsg.mingler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinglerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
